4952d928979a93dfb08dd7f1bf33ae4c2e94fe17
TravisTX <travistx@gmail.com>
2016-05-29 21:22:03 -0400

    enums

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,22 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  mode: any; // todo: enum. valid options:  present, input, gameOver 
-  potentialAnswers: any[] = ['red', 'yellow', 'green', 'blue'];
-  answerList: any[] = [];
-  guessList: any[] = [];
+  stateEnum: any = stateEnum;
+  colorEnum: any = colorEnum;
+  state: stateEnum;
+  answerList: colorEnum[] = [];
+  guessList: colorEnum[] = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.mode = 'present';
     this.addAnswer();
     this.presentAnswers();
   }
 
-  click(color: any) {
-    if (this.mode !== 'input') {
+  click(color: colorEnum) {
+    if (this.state !== stateEnum.input) {
       return;
     }
 
@@ -31,20 +30,20 @@ export class GameComponent implements OnInit {
   }
 
   addAnswer() {
-    var newAnswer = this.potentialAnswers[Math.floor(Math.random() * this.potentialAnswers.length)];
+    let newAnswer = this.getRandomElementOfEnum<colorEnum>(colorEnum);
     this.answerList.push(newAnswer);
   }
 
   checkAnswers() {
-    var vm = this;
-    this.guessList.forEach(function (value: any, index: number, array: any[]) {
+    let vm = this;
+    this.guessList.forEach(function (value: colorEnum, index: number, array: any[]) {
       if (vm.answerList[index] !== value) {
-        vm.mode = 'gameOver';
+        vm.state = stateEnum.gameOver;
         alert('you lose');
       }
     });
 
-    if (this.mode === 'gameOver') {
+    if (this.state === stateEnum.gameOver) {
       return;
     }
 
@@ -56,8 +55,32 @@ export class GameComponent implements OnInit {
   }
 
   presentAnswers() {
-    this.mode = 'present';
-    this.mode = 'input';
+    this.state = stateEnum.present;
+    this.state = stateEnum.input;
   }
 
+  getRandomElementOfEnum<E>(e: any): E {
+    let keys = Object.keys(e),
+      index = Math.floor(Math.random() * keys.length),
+      k = keys[index];
+    if (typeof e[k] === 'number')
+      return <any>e[k];
+    return <any>parseInt(k, 10);
+  }
+
+
+
 }
+
+enum stateEnum {
+  present,
+  input,
+  gameOver
+};
+
+enum colorEnum {
+  red,
+  yellow,
+  green,
+  blue
+};
