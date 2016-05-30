8e04ca0fe31a1eeccbe6b15f30af1dfa00b34bce
TravisTX <travistx@gmail.com>
2016-05-29 20:03:00 -0400

    add buttons

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['game.component.css']
 })
 export class GameComponent implements OnInit {
-
+  mode: any; // todo: enum. valid options:  present, input, gameOver 
+  potentialAnswers: any[] = ['red', 'yellow', 'green', 'blue'];
+  answerList: any[] = [];
+  guessList: any[] = [];
+  
   constructor() {}
 
   ngOnInit() {
+    this.mode = 'present';
+  }
+  
+  click(color: any) {
+    this.guessList.push(color);
+  }
+  
+  addAnswer() {
+    var newAnswer = this.potentialAnswers[Math.floor(Math.random() * this.potentialAnswers.length)];
+    this.answerList.push(newAnswer);
+  }
+  
+  presentAnswers() {
+    
   }
 
 }
